ec6a368f3e6a56f72a0f3c8a6f8a68d7241f8953
Shawn <changoloco.evg@gmail.com>
2025-05-10 22:09:14 +0000

    feat: initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>
+  Welcome to SvelteKit with TypeScript!
+</h1>
+
+<p>
+  This is a simple SvelteKit application that demonstrates how to use TypeScript with Svelte.
+</p>
---
+++ uno.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'unocss/vite';
+import {
+	presetWind3,
+	presetWebFonts,
+	transformerDirectives,
+	transformerVariantGroup
+} from 'unocss';
+
+export default defineConfig({
+	transformers: [transformerDirectives({ applyVariable: '--uno' }), transformerVariantGroup()],
+	presets: [
+		presetWind3(),
+		presetWebFonts({
+			provider: 'fontshare',
+			fonts: {
+				chillax: "Chilax"
+			}
+		})
+	]
+});
