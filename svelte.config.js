33888f93bcad3182ad4a4a3a285f74bd8bb65a11
Shawn <changoloco.evg@gmail.com>
2025-05-11 00:50:41 +0000

    feat: prerender site

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
+import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const config = {
+export default {
 	preprocess: [vitePreprocess(), mdsvex()],
 	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.svx']
 };
-
-export default config;
